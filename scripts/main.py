d685627529ec0d1199e744e5ee47677a74fcd4a5
Kinda Fayad <kindafayad@gmail.com>
2024-03-24 18:48:09 -0400

    c

--- scripts/main.py
+++ scripts/main.py
@@ -12,9 +12,10 @@ from population import population_data
 percent_offset_lb = 0.50
 percent_offset_ub = 1.50
 
-selected_year = int(input("Please select a year (2010, 2020, 2030, 2040): "))
-while selected_year not in [2010, 2020, 2030, 2040]:
-    selected_year = int(input("Invalid selection. Please choose a valid year: "))
+# selected_year = int(input("Please select a year (2010, 2020, 2030, 2040): "))
+# while selected_year not in [2010, 2020, 2030, 2040]:
+    # selected_year = int(input("Invalid selection. Please choose a valid year: "))
+selected_year = 2010
 
 # Filter data for the selected year
 # Filter the population data for the selected year
@@ -42,7 +43,8 @@ def run_model(district_numb):
     x = model.addVars(district_numb, len(counties_geoId), vtype=GRB.BINARY, name="x")
 
     # District population per district
-    z = model.addVars(district_numb, vtype=GRB.INTEGER, ub=(ideal_pop * percent_offset_ub), lb=(ideal_pop * percent_offset_lb), name="z")
+    z = model.addVars(district_numb, vtype=GRB.INTEGER, name="z")
+    # z = model.addVars(district_numb, vtype=GRB.INTEGER, ub=(ideal_pop * percent_offset_ub), lb=(ideal_pop * percent_offset_lb), name="z")
     z_min = model.addVar(vtype=GRB.INTEGER, name="z_min")
     z_max = model.addVar(vtype=GRB.INTEGER, name="z_max")
 
@@ -61,17 +63,19 @@ def run_model(district_numb):
     for i in range(len(counties_geoId)):
         for j in range(len(counties_geoId)):
             if check_county_adjacency(counties_geoId[i], counties_geoId[j]):
-                # print(f"Adding adjacency constraint for {counties_geoId[i]} and {counties_geoId[j]}")
                 for k in range(district_numb):
-                    model.addConstr(quicksum((x[k, i], x[k, j])) == 2 , f"Contiguity_{i}_{j}_{k}")
+                    model.addConstr(quicksum([x[k, i], x[k, j]]) == 2 , f"Contiguity_{i}_{j}_{k}")
 
     # Add population constraints
     for i in range(district_numb):
         model.addConstr(quicksum(x[i, j] * int(filtered_population_data[filtered_population_data['GEOID'] == geoid]["Total"].iloc[0]) for j, geoid in enumerate(counties_geoId)) == z[i], f"district_{i}_pop_equals_aggregate_county_pop")
 
     # Set Auxiliary Variables
-    model.addConstr(z_min == min_(z[i] for i in range(district_numb)), "set_z_min")
-    model.addConstr(z_max == max_(z[i] for i in range(district_numb)), "set_z_max")
+    model.addGenConstrMin(z_min, z, name="set_z_min")
+    model.addGenConstrMax(z_max, z, name="set_z_max")
+
+    # model.addConstr(z_min == min_(z[i] for i in range(district_numb)), "set_z_min")
+    # model.addConstr(z_max == max_(z[i] for i in range(district_numb)), "set_z_max")
 
     # Optimize the model
     model.optimize()
@@ -87,7 +91,7 @@ def run_model(district_numb):
     else:
         print("No feasible solution found.")
 
-for i in range (7, 10, 1):
+for i in range (7, 8, 1):
     print(f"Running model for iteration {i}")
     run_model(i)
     # time.sleep(3)
